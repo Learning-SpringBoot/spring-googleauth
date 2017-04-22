309afe157d7d0578ebfe7f966822a8e86bd4080a
unknown <sambitmishra987@gmail.com>
2017-04-22 13:01:26 +0530

    simple: very basic static app with just a home page and unconditional login.

---
+++ src/main/java/com/speed/mixer/SpringGoogleauthApplication.java
@@ -0,0 +1,14 @@
+package com.speed.mixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SpringGoogleauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGoogleauthApplication.class, args);
+	}
+}
